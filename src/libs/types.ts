a317981907825a0066cea6ccc196c2d9344eaea8
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-08-08 15:08:42 +0700

    Update types.ts

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,5 +1,27 @@
 // You can define interfaces for all React components here.
 // Do not forget to export properly.
 
-interface ReplyProps {}
+interface ReplyProps {
+    ImagePath: string;
+    username: string;
+    replyTitle: string;
+    likes: number;
+}
 export type { ReplyProps };
+
+interface CommentProps {
+    username: string;
+    ImagePath: string;
+    commentTitle: string;
+    likes: number;
+    replies: ReplyProps[];
+}
+export type { CommentProps };
+
+interface PostOwnerProps {
+    username: string;
+    userImagePath: string;
+    Text: string;
+    like: number;
+}
+export type { PostOwnerProps };
